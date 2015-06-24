8cab80217e3629d52efb1254b8e2e2e482f99d05
Niels Ballemans <uniels@users.noreply.github.com>
2015-06-24 22:14:27 +0200

    One Digit Prepared

--- example/index.php
+++ example/index.php
@@ -8,29 +8,10 @@
         <script src="../numerify.js"></script>
     </head>
     <body>
-        <div id="number" class="numerify-container">
-            <div class="numerify-a"></div>
-            <div class="numerify-b"></div>
-            <div class="numerify-c"></div>
-            <div class="numerify-d"></div>
-            <div class="numerify-e"></div>
-            <div class="numerify-f"></div>
-            <div class="numerify-g"></div>
+        <div id="number">
         </div>
-        <select id="selectbox">
-            <?php for($i = 0; $i < 10; $i++){
-                echo "<option val='$i'>$i</option>";
-            }
-            ?>
-        </select>
     </body>
     <script>
-    numerify('number');
-
-    $('#selectbox').change(function() {
-        $('#number').data("number",$(this).val());
-        numerifySet($('#number'),$(this).val());
-    });
-
+    numerifyObject('number');
     </script>
 </html>
--- numerify.js
+++ numerify.js
@@ -1,68 +1,117 @@
-function numerify(objectid)
+function numerify($placeholder,total)
 {
-    var $container = $('#'+objectid);
-    console.log($container.length);
-    var digits = $();
 
 }
 
-function numerifyReset($object)
+function numerifyObject($objectid)
+{
+    var $container = $('#'+objectid);
+    if($container.length === 1){
+        var digits = [];
+        for (var n = 0; n < 8; n++) {
+            digits.push( $('<div />').addClass('numerify-'+String.fromCharCode(97 + n)) );
+        };
+        $container.addClass('numerify-container').append(digits);
+        $container.click(function(){
+            numerifyUp( $(this) );
+        });
+        numerifySetNumber($container,'0')
+    } else console.log('Error on Numerify. "'+objectid+'" is not an object.');
+    numerifyShowDigits($container);
+}
+
+function numerifyHideDigits($object)
 {
     $object.find('.numerify-show').removeClass('numerify-show');
 }
 
-function numerifySet($object,number)
+function numerifyShowDigits($object)
 {
-    numerifyReset($object);
-    switch ( parseInt(number) ){
-        case 8:
-            numerifyShowDigit($object,"e");
-        case 9:
-            numerifyShowDigit($object,"b");
-        case 3:
-            numerifyShowDigit($object,"d");
-            numerifyShowDigit($object,"g");
-        case 7:
-            numerifyShowDigit($object,"a");
-        case 1:
-            numerifyShowDigit($object,"c");
-            numerifyShowDigit($object,"f");
-            break;
-        case 6:
-            numerifyShowDigit($object,"e");
-        case 5:
-            numerifyShowDigit($object,"a");
-            numerifyShowDigit($object,"b");
-            numerifyShowDigit($object,"d");
-            numerifyShowDigit($object,"f");
-            numerifyShowDigit($object,"g");
-            break;
-        case 4:
-            numerifyShowDigit($object,"c");
-            numerifyShowDigit($object,"b");
-            numerifyShowDigit($object,"d");
-            numerifyShowDigit($object,"f");
-            break;
-        case 0:
-            numerifyShowDigit($object,"a");
-            numerifyShowDigit($object,"b");
-            numerifyShowDigit($object,"c");
-            numerifyShowDigit($object,"e");           
-            numerifyShowDigit($object,"f");
-            numerifyShowDigit($object,"g");
-            break;
-        case 2:
-            numerifyShowDigit($object,"a");
-            numerifyShowDigit($object,"c");
-            numerifyShowDigit($object,"e");           
-            numerifyShowDigit($object,"g"); 
-        default:
-            numerifyShowDigit($object,"d");
-            break;
+    numerifyHideDigits($object);
+    var number = numerifyGetNumber($object);
+    if( !isNaN(number) && (number >= 0 && number < 10) ){
+        $object.data('number',number);
+        switch ( number ){
+            case 8:
+                numerifyShowDigit($object,"e");
+            case 9:
+                numerifyShowDigit($object,"b");
+            case 3:
+                numerifyShowDigit($object,"d");
+                numerifyShowDigit($object,"g");
+            case 7:
+                numerifyShowDigit($object,"a");
+            case 1:
+                numerifyShowDigit($object,"c");
+                numerifyShowDigit($object,"f");
+                break;
+            case 6:
+                numerifyShowDigit($object,"e");
+            case 5:
+                numerifyShowDigit($object,"a");
+                numerifyShowDigit($object,"b");
+                numerifyShowDigit($object,"d");
+                numerifyShowDigit($object,"f");
+                numerifyShowDigit($object,"g");
+                break;
+            case 4:
+                numerifyShowDigit($object,"c");
+                numerifyShowDigit($object,"b");
+                numerifyShowDigit($object,"d");
+                numerifyShowDigit($object,"f");
+                break;
+            case 0:
+                numerifyShowDigit($object,"a");
+                numerifyShowDigit($object,"b");
+                numerifyShowDigit($object,"c");
+                numerifyShowDigit($object,"e");           
+                numerifyShowDigit($object,"f");
+                numerifyShowDigit($object,"g");
+                break;
+            case 2:
+                numerifyShowDigit($object,"a");
+                numerifyShowDigit($object,"c");
+                numerifyShowDigit($object,"e");           
+                numerifyShowDigit($object,"g"); 
+            default:
+                numerifyShowDigit($object,"d"); 
+                break;
+        }
+    } else {
+        numerifyShowDigit($object,"d");
     }
 }
 
 function numerifyShowDigit($object,digit)
 {
     $object.find('.numerify-'+digit).addClass("numerify-show");
+}
+
+function numerifySetNumber($object,num)
+{
+    var number = parseInt(num);
+    $object.data('number',number);
+}
+
+function numerifyGetNumber($object)
+{
+    return parseInt($object.data('number')) || 0;
+}
+
+function numerifyUp($object)
+{
+    var number = numerifyGetNumber($object);
+    number++;
+    if( number == 10 ) number = 0;
+    numerifySetNumber($object,number);
+    numerifyShowDigits($object);
+}
+
+function numerifyDown($object)
+{
+    var number = numerifyGetNumber($object);
+    number--;
+    if( number < 0 ) number = 9;
+    numerifySetNumber($object,number);
+    numerifyShowDigits($object);
 }
